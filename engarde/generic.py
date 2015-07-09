75cc68e26be8bdb24020a30a9a88c912ed8ccd2f
Tom Augspurger <tom.w.augspurger@gmail.com>
2015-07-09 09:01:49 -0500

    ENH: Slight error reporting for generic

--- engarde/generic.py
+++ engarde/generic.py
@@ -28,17 +28,32 @@ def verify(df, check, *args, **kwargs):
         same as the input.
     """
     result = check(df, *args, **kwargs)
-    assert result
+    try:
+        assert result
+    except AssertionError as e:
+        msg = '{} is not true'.format(check.__name__)
+        e.args = (msg, df)
+        raise
     return df
 
 def verify_all(df, check, *args, **kwargs):
     result = check(df, *args, **kwargs)
-    assert np.all(result)
+    try:
+        assert np.all(result)
+    except AssertionError as e:
+        msg = "{} not true for all".format(check.__name__)
+        e.args = (msg, df[~result])
+        raise
     return df
 
 def verify_any(df, check, *args, **kwargs):
     result = check(df, *args, **kwargs)
-    assert np.any(result)
+    try:
+        assert np.any(result)
+    except AssertionError as e:
+        msg = '{} not true for any'.format(check.__name__)
+        e.args = (msg, df)
+        raise
     return df
 
 # ---------------
