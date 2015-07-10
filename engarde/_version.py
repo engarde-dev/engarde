4244642cd9ee3955eb3eccc3cf2fb7a058acfcad
Tom Augspurger <tom.w.augspurger@gmail.com>
2015-07-10 15:03:06 -0500

    tag prefix

--- engarde/_version.py
+++ engarde/_version.py
@@ -36,7 +36,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "None"
+    cfg.tag_prefix = "''"
     cfg.parentdir_prefix = "engarde-"
     cfg.versionfile_source = "engarde/_version.py"
     cfg.verbose = False
